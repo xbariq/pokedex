b5807fd7af206d35122de989fffd7751dbf923c4
Bariq Talib <btali086@uottawa.ca>
2022-09-22 08:42:40 -0400

    Goal 2: Cache results locally

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -22,27 +22,42 @@ function App() {
 
   const GET_POKEMON_ENDPOINT= "https://pokeapi.co/api/v2/pokemon";
 
+  const getPokemonDataFromApi=async (pokemonName:string) => {
+    //call pokemon endpoint
+   const response = await fetch(`${GET_POKEMON_ENDPOINT}/${pokemonName}`); 
+   console.log(response);
+   if (response.status === 200) {
+     const data = await response.json();
+     console.log(data);
+     setPokemonData(data);
+     setStatus(AppStatus.SUCCESS);
+
+     // Save Pokemon data to local storage.
+     localStorage.setItem(searchText, JSON.stringify(data));
+     
+   } else if (response.status === 404) {
+     console.log("Pokemon not found");
+     setStatus(AppStatus.NOT_FOUND);
+   }
+  };
+
 
   const onClickSearch= async (e:any)=> {
     e.preventDefault();
-    console.log(`Searching for ${searchText}`);
+    const searchTextLower= searchText.toLowerCase();
+    console.log(`Searching for ${searchTextLower}`);
     setStatus(AppStatus.LOADING);
 
-    // Call the pokemin endpoint
-    const response = await fetch(`${GET_POKEMON_ENDPOINT}/${searchText}`); 
-    console.log(response);
-    if (response.status === 200) {
-      const data = await response.json();
-      console.log(data);
-      setPokemonData(data);
+    //Check if pokemon data is in local storage. 
+    const pokemonDataFromLocalStorage = localStorage.getItem(searchTextLower);
+    if (pokemonDataFromLocalStorage) { 
+      console.log("Pokemon data found in local storage");
+      setPokemonData(JSON.parse(pokemonDataFromLocalStorage));
       setStatus(AppStatus.SUCCESS);
+    } else {
+      console.log("Pokemon data not found in local storage");
+      await getPokemonDataFromApi(searchTextLower);
 
-      // Save Pokemon data to local storage.
-      localStorage.setItem(searchText, JSON.stringify(data));
-      
-    } else if (response.status === 404) {
-      console.log("Pokemon not found");
-      setStatus(AppStatus.NOT_FOUND);
     }
   };
 
