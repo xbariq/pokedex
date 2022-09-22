8945bd8ab7585310353dd04e1e008669fb778066
Bariq Talib <btali086@uottawa.ca>
2022-09-22 05:37:52 -0400

    Added Search Button

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,8 +3,32 @@ import logo from './logo.svg';
 import './index.css';
 
 function App() {
+
+  const [searchText, setSearchText]=  React.useState(null);
+  const GET_POKEMON_ENDPOINT= "https://pokeapi.co/api/v2/";
+
+  const onClickSearch= (e:any)=>{
+    e.preventDefault();
+    console.log("Searching for Pokemon...")
+
+
+
+    // Call the pokemin endpoint
+
+
+  }
+
+  const searchBox= (
+     <div>
+      <form> 
+    <input type="text" placeholder='Search for a Pokemon'/>
+    <button onClick={onClickSearch}>Search</button>
+    </form>
+  </div>
+  );
   return (
     <div className="text-2xl"> POKEDEX
+    {searchBox}
       
     </div>
   );
