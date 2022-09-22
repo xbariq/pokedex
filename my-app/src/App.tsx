8b9e92e34235bc9dd69f22e4d3321c22ebebe0e2
Bariq Talib <btali086@uottawa.ca>
2022-09-22 06:29:40 -0400

    Called pokemon endpoint (Api)

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,36 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './index.css';
 
+
 function App() {
 
-  const [searchText, setSearchText]=  React.useState(null);
-  const GET_POKEMON_ENDPOINT= "https://pokeapi.co/api/v2/";
+  const [searchText, setSearchText]=  React.useState("");
+  const GET_POKEMON_ENDPOINT= "https://pokeapi.co/api/v2/pokemon";
 
-  const onClickSearch= (e:any)=>{
+  const onClickSearch= async (e:any)=> {
     e.preventDefault();
-    console.log("Searching for Pokemon...")
-
-
+    console.log(`Searching for ${searchText}`);
 
     // Call the pokemin endpoint
-
-
-  }
+    const response = await fetch(`${GET_POKEMON_ENDPOINT}/${searchText}`); 
+    console.log(response);
+  };
 
   const searchBox= (
-     <div>
-      <form> 
-    <input type="text" placeholder='Search for a Pokemon'/>
-    <button onClick={onClickSearch}>Search</button>
-    </form>
-  </div>
-  );
-  return (
-    <div className="text-2xl"> POKEDEX
-    {searchBox}
-      
+      <div>
+        <form> 
+      <input type="text" placeholder="Search for a Pokemon" onChange={(e) => {setSearchText(e.currentTarget.value);
+            }}/>
+      <button onClick={onClickSearch}>Search</button>
+      </form>
     </div>
-  );
-}
+    );
+    return (
+      <div className="text-2xl"> POKEDEX
+      {searchBox}
+        
+      </div>
+    );
+  }
 
 export default App;
